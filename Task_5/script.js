2084531c1c1c01f0a1ae281c490dd0f237a2b5fa
SkinFish <crazygreen15@gmail.com>
2017-03-11 18:45:13 +0200

    Task 5 completed

---
+++ Task_5/script.js
@@ -0,0 +1,16 @@
+function forLancelot(arr) {
+	var woman = 0;
+	var man = 0;
+	for (var i = 0;i<arr.length;i++) {
+		if (arr[i] == 1) {
+			man++;
+		}else if (arr[i] == -1) {
+			woman++;
+		}
+	}
+	if (man > woman) {
+		return true;
+	}else {
+		return false;
+	}
+}
